0cce87e55bb7a5bfa713ce7e16bcde6c370fc33c
SohailAQ <sohail.developes@gmail.com>
2022-06-06 02:58:22 +0530

    Project Basics

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,15 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
 STATIC_ROOT = 'vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- app/todos/management/commands/wait_for_db.py
+++ app/todos/management/commands/wait_for_db.py
@@ -1,3 +1,6 @@
+"""
+Django command to wait for the database to be available.
+"""
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -5,20 +8,20 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-	"""Code Snippet to wait for database"""
+    """Django command to wait for database."""
 
-	def handle(self, *args, **options):
-		"""Entrypoint"""
-		self.stdout.write("Waiting for database...")
-		db_up = False
-		while db_up is False:
-			try:
-				self.check(databases=['default'])
-				db_up = True
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
 
-			except (Psycopg2OpError, OperationalError):
-				self.stdout.write("Database unavailable, waiting 1 second...")
-				time.sleep(1)
-
-		self.stdout.write(self.style.SUCCESS("Database ready!"))
+        self.stdout.write(self.style.SUCCESS('Database available!'))
